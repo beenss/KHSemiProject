0a11d87a3b35dc38a0016391cb33c501517e4f1e
saetbeyol <saetbyeoling@gmail.com>
2022-05-29 17:53:11 +0900

    reviewDTO

---
+++ src/main/java/com/boribob/dto/ReviewDTO.java
@@ -0,0 +1,66 @@
+package com.boribob.dto;
+
+public class ReviewDTO {
+	
+	private int seqReview;
+	private int productCode;
+	private String id;
+	private String reviewTitle;
+	private String reviewContent;
+	private String reviewDate;
+	
+	public ReviewDTO() {}
+	public ReviewDTO(int seqReview, int productCode, String id, String reviewTitle, 
+			String reviewContent, String reviewDate) {
+		super();
+		this.seqReview = seqReview;
+		this.productCode = productCode;
+		this.id = id;
+		this.review_title = reviewTtle;
+		this.review_content = reviewContent;
+		this.review_date = reviewDate;
+	}
+	public int getSeqReview() {
+		return seqReview;
+	}
+	public void setSeqReview(int seqReview) {
+		this.seqReview = seqReview;
+	}
+	public int getProdictCode() {
+		return productCode;
+	}
+	public void setProdictCode(int prodictCode) {
+		this.productCode = prodictCode;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getReviewTitle() {
+		return reviewTitle;
+	}
+	public void setReviewTitle(String reviewTitle) {
+		this.reviewTitle = reviewTitle;
+	}
+	public String getReviewContent() {
+		return reviewContent;
+	}
+	public void setReview_content(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+	public String getReviewDate() {
+		return reviewDate;
+	}
+	public void setReview_date(String reviewDate) {
+		this.reviewDate = review_date;
+	}
+	@Override
+	public String toString() {
+		return "ReviewDTO [seqReview=" + seqReview + ", prodictCode=" + productCode + ", id=" + id
+				+ ", reviewTitle=" + reviewTitle + ", reviewContent=" + reviewContent + ", reviewDate="
+				+ reviewDate + "]";
+	}
+	
+}
