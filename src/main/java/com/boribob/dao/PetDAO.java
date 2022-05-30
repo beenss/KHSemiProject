cfb6c9b8a65decfc438c6990d7cca01a467e0858
NK590 <nathankim0917@gmail.com>
2022-05-30 14:16:45 +0900

    update PetDAO SubscribeDAO

--- src/main/java/com/boribob/dao/PetDAO.java
+++ src/main/java/com/boribob/dao/PetDAO.java
@@ -2,6 +2,7 @@ package com.boribob.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -9,6 +10,7 @@ import javax.naming.InitialContext;
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 import com.boribob.dto.PetDTO;
+import com.boribob.utils.Algorithms;
 
 public class PetDAO {
 	private BasicDataSource bds;
@@ -28,6 +30,7 @@ public class PetDAO {
 		return bds.getConnection();
 	}
 	
+	// 펫 정보 입력
 	public int insert(PetDTO dto) throws Exception {
 		String sql = "insert into tbl_pet values (?, ?, ?, ?, ?, ?, ?)";
 		try(Connection con = this.getConnection();
@@ -45,4 +48,35 @@ public class PetDAO {
 			return rs;
 		}
 	}
+	
+	// 펫의 정보를 비트 배열로 저장
+	public ArrayList<Integer> getPetStatus(PetDTO dto) throws Exception {
+		ArrayList<Integer> petDetails = new ArrayList<>();
+		
+		// 인덱스 순서대로 견/묘 구분, 나이, 알러지 종류, 몸무게
+		// 견/묘 구분 입력
+		if (dto.getPetType() == "dog") petDetails.add(0); // 개
+		else if (dto.getPetType() == "cat") petDetails.add(1); // 고양이
+		
+		// 나이 입력
+		if (dto.getPetAge() <= 5) petDetails.add(0); // 5살 이하
+		else petDetails.add(1); // 6살 이상
+		
+		// 알러지 종류 입력
+		petDetails.add(dto.getPetAllergy()); // 0이면 없음, 1이면 있음
+		
+		// 몸무게 입력
+		if (dto.getPetWeight() <= 5) petDetails.add(0); // 5kg 이하
+		else petDetails.add(1); // 6kg 이상
+		
+		return petDetails;
+	}
+	
+	// 펫의 비트배열 정보를 숫자(상품번호)로 치환
+	public int getProductNum(ArrayList<Integer> petDetails) throws Exception {
+		Algorithms algorithm = new Algorithms();
+		return algorithm.bitmask(petDetails, 0);
+	}
+	
+	
 }
--- src/main/java/com/boribob/utils/Algorithms.java
+++ src/main/java/com/boribob/utils/Algorithms.java
@@ -3,10 +3,16 @@ package com.boribob.utils;
 import java.util.ArrayList;
 
 public class Algorithms {
+	// 펫 정보 비트 배열을 비트 숫자로 치환
 	public int bitmask(ArrayList<Integer> petDetails, int targetNumber) {
 		for (int i = 0; i < petDetails.size(); i++) {
 			targetNumber |= (petDetails.get(i) << i);
 		}
 		return targetNumber;
+	}
+	
+	// 특정 위치 비트 정보 꺼내기
+	public boolean checkBit(int inputNumber, int targetDigit, int targetNumber) {
+		return (targetNumber & (inputNumber << targetDigit)) == (inputNumber << targetDigit);
 	}
 }
