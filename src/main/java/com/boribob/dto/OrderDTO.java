94646ae148da90228ac237d04b62781bef438048
cszzang89@gamail.com <cszzang89@gmail.com>
2022-05-30 10:46:37 +0900

    5/30 월 OrderDTO, OrderDAO  수

--- src/main/java/com/boribob/dto/OrderDTO.java
+++ src/main/java/com/boribob/dto/OrderDTO.java
@@ -1,160 +1,158 @@
 package com.boribob.dto;
 
 public class OrderDTO {
-	
-
-	
-			
-				
-		 	private Long orderNo; //order_no number 주문번호 primary key 
-		    private String memberId; //member_id varchar2(100) 회원명 forin key 
-		    private String orderName; //order_name varchar2(100) 수취인 이름 
-		    private String orderContact; //order_contact varchar2(100) 수취인 연락처 
-		    private String orderAddress; // order_address varchar2(100) 수취인 주소 
-		    private String orderMsg; //  order_msg varchar2(200) 주문 메시지 
-		    private String postMsg; //post_msg varchar2(200) 배송 매시지 
-		    private String payNumber; // pay_number number 결제번호 
-		    private String productCode; //product_code number 상품번호 
-		    private String totalPay; // total_pay number 결제금액 
-		    private String card; //card VARCHAR(100)  카드 0000
-		    
-		    
-		    
-		    
-		    public OrderDTO(Long orderNo, String memberId, String orderName, String orderContact, String orderAddress,
-					String orderMsg, String postMsg, String payNumber, String productCode, String totalPay, String card) {
-				
-				this.orderNo = orderNo;
-				this.memberId = memberId;
-				this.orderName = orderName;
-				this.orderContact = orderContact;
-				this.orderAddress = orderAddress;
-				this.orderMsg = orderMsg;
-				this.postMsg = postMsg;
-				this.payNumber = payNumber;
-				this.productCode = productCode;
-				this.totalPay = totalPay;
-				this.card = card;
-			}
-
-
-			public OrderDTO() {}
-
-
-			public Long getOrderNo() {
-				return orderNo;
-			}
-
-
-			public void setOrderNo(Long orderNo) {
-				this.orderNo = orderNo;
-			}
-
-
-			public String getMemberId() {
-				return memberId;
-			}
-
-
-			public void setMemberId(String memberId) {
-				this.memberId = memberId;
-			}
-
-
-			public String getOrderName() {
-				return orderName;
-			}
-
-
-			public void setOrderName(String orderName) {
-				this.orderName = orderName;
-			}
-
-
-			public String getOrderContact() {
-				return orderContact;
-			}
-
-
-			public void setOrderContact(String orderContact) {
-				this.orderContact = orderContact;
-			}
-
-
-			public String getOrderAddress() {
-				return orderAddress;
-			}
-
-
-			public void setOrderAddress(String orderAddress) {
-				this.orderAddress = orderAddress;
-			}
-
-
-			public String getOrderMsg() {
-				return orderMsg;
-			}
-
-
-			public void setOrderMsg(String orderMsg) {
-				this.orderMsg = orderMsg;
-			}
-
-
-			public String getPostMsg() {
-				return postMsg;
-			}
-
-
-			public void setPostMsg(String postMsg) {
-				this.postMsg = postMsg;
-			}
-
-
-			public String getPayNumber() {
-				return payNumber;
-			}
-
-
-			public void setPayNumber(String payNumber) {
-				this.payNumber = payNumber;
-			}
-
-
-			public String getProductCode() {
-				return productCode;
-			}
-
-
-			public void setProductCode(String productCode) {
-				this.productCode = productCode;
-			}
-
-
-			public String getTotalPay() {
-				return totalPay;
-			}
-
-
-			public void setTotalPay(String totalPay) {
-				this.totalPay = totalPay;
-			}
-
-
-			public String getCard() {
-				return card;
-			}
-
-
-			public void setCard(String card) {
-				this.card = card;
-			}
-
-		    
 		
 			
-		    
-		    
-		}
+	private int orderNo; //order_no number 주문번호 primary key
+ 	private int seqSubscribe;
+	private String orderName;//수취인 이름 
+    private String orderPhone; // 수취인 연락처 
+    private String orderPostCode; //  수취인 우편번호 
+    private String orderAddress; //  수취인 주소  
+    private String orderDetailAddress; //수취인 상세주소 
+    private String orderMsg; // 주문메시지 
+    private String postMsg; //  배송메시지
+    
+  
+    
+	public OrderDTO(){}
 
 
+
+	public OrderDTO(int orderNo, int seqSubscribe, String orderName, String orderPhone, String orderPostCode,
+			String orderAddress, String orderDetailAddress, String orderMsg, String postMsg) {
+		super();
+		this.orderNo = orderNo;
+		this.seqSubscribe = seqSubscribe;
+		this.orderName = orderName;
+		this.orderPhone = orderPhone;
+		this.orderPostCode = orderPostCode;
+		this.orderAddress = orderAddress;
+		this.orderDetailAddress = orderDetailAddress;
+		this.orderMsg = orderMsg;
+		this.postMsg = postMsg;
+	}
+
+
+
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+
+
+
+	public int getSeqSubscribe() {
+		return seqSubscribe;
+	}
+
+
+
+	public void setSeqSubscribe(int seqSubscribe) {
+		this.seqSubscribe = seqSubscribe;
+	}
+
+
+
+	public String getOrderName() {
+		return orderName;
+	}
+
+
+
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+
+
+	public String getOrderPhone() {
+		return orderPhone;
+	}
+
+
+
+	public void setOrderPhone(String orderPhone) {
+		this.orderPhone = orderPhone;
+	}
+
+
+
+	public String getOrderPostCode() {
+		return orderPostCode;
+	}
+
+
+
+	public void setOrderPostCode(String orderPostCode) {
+		this.orderPostCode = orderPostCode;
+	}
+
+
+
+	public String getOrderAddress() {
+		return orderAddress;
+	}
+
+
+
+	public void setOrderAddress(String orderAddress) {
+		this.orderAddress = orderAddress;
+	}
+
+
+
+	public String getOrderDetailAddress() {
+		return orderDetailAddress;
+	}
+
+
+
+	public void setOrderDetailAddress(String orderDetailAddress) {
+		this.orderDetailAddress = orderDetailAddress;
+	}
+
+
+
+	public String getOrderMsg() {
+		return orderMsg;
+	}
+
+
+
+	public void setOrderMsg(String orderMsg) {
+		this.orderMsg = orderMsg;
+	}
+
+
+
+	public String getPostMsg() {
+		return postMsg;
+	}
+
+
+
+	public void setPostMsg(String postMsg) {
+		this.postMsg = postMsg;
+	}
+
+
+	
+
+	
+	
+	
+	
+	    
+	    
+	    
+	    
+		
+	    
+	    
+	}
