bc774b48486d2d965e587f40c6f94c1c336566f5
NK590 <nathankim0917@gmail.com>
2022-06-09 10:51:30 +0900

    update HomeController

--- src/main/java/com/boribob/controller/HomeController.java
+++ src/main/java/com/boribob/controller/HomeController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/home")
 public class HomeController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-<<<<<<< HEAD
 		doAction(request, response);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -19,14 +18,4 @@ public class HomeController extends HttpServlet {
 	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("/index.jsp");
 	}
-=======
-		response.sendRedirect("/index.jsp");
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		doGet(request, response);
-	}
-
->>>>>>> a3b8848ac17381e2cf2981ae245d31f0cb2f06d8
 }
