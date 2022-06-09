3be0c762c2e35d2dad910e14bc36f1de911b30ba
seongmin <sungpar1994@naver.com>
2022-06-09 10:40:34 +0900

    modified file

--- src/main/java/com/boribob/controller/HomeController.java
+++ src/main/java/com/boribob/controller/HomeController.java
@@ -7,19 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
-public class LoginController extends HttpServlet {
+@WebServlet("/home")
+public class HomeController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 로그인 페이지로 이동
-		response.sendRedirect("/login.jsp");
+		response.sendRedirect("/index.jsp");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	
 		doGet(request, response);
 	}
-	
-	
-	
 
 }
